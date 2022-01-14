3195bc57a46f9cba9ddc5b88aa69bfb548cc2b7e
Zhupar Adamova <zhuparadamova@gmail.com>
2022-01-14 19:35:49 +0600

    ws-server

--- app.py
+++ app.py
@@ -1,19 +1,68 @@
 import asyncio
 import websockets
+import aiohttp
+import time
 
-tasks =['news1', 'news2', 'news3']
-connected = set()
+url = 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=7ef9503baea4428b9816689d82f0bc1c'
+tasks =[]
+
+
+
+async def fetch_news(url, session):  
+    async with session.get(url) as response:
+        data = await response.json()
+        return data["articles"]
+        
+
+
+
 
 async def server(websocket, path):
 
-    await websocket.recv()
-    await websocket.send('news')
+    connected = set()
+    print('received')
+    while True:
+        
+        connected.add(websocket)
+
+        try:
+            for conn in connected:
+                print(conn)
+                if tasks:
+                    for task in tasks:
+                        to_send = task["title"] + '\n' + task["url"] +'\n\n'
+                        await conn.send(to_send)
+        finally:
+        # Unregister.
+            connected.remove(websocket)
+
+
+
+        async with aiohttp.ClientSession() as session:            
+            print('in session')
+            articles = await fetch_news(url, session)
+            count = 0
+            for article in articles:
+                if article not in tasks:
+                    if count <=10:
+                        tasks.append(article)
+                        count += 1
+                    else:
+                        tasks.pop(0)
+                        # print(*tasks)
+            time.sleep(60)
+        
+        
+        
+        
 
 
 
 async def main():
     async with websockets.serve(server, "localhost", 8080):
         await asyncio.Future()  # run forever
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 // Создаёт WebSocket - подключение.
-
+let news = ''
 const socket = new WebSocket('ws://localhost:8080');
 
 // Соединение открыто
 socket.addEventListener('open', function (event) {
-    console.log('opened')
+    console.log('opened');
+    socket.send('message');
 });
 
 // Наблюдает за сообщениями
-socket.addEventListener('message', function (event) {
-    p.before(event.data)
+let fromServer = socket.addEventListener('message', function (event) {
+    news += '<blockquote>Latest news: '+ new Date() + "\n\n" + event.data +'</blockquote>';
+    document.querySelector("#p").innerHTML = news;
     console.log('Message from server ', event.data);
 });
 
-const sndMsg = () =>{
-    socket.send('message');
-    console.log('clicked')
-};
+
